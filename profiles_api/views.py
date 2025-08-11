3f9efd9be4148b9d1551f025b393a50bef265289
Klecianny Melo <klecianny.melo@quorum.us>
2025-08-11 20:38:00 -0300

    :bug: lint error

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -90,10 +90,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
-    
 
-    class UserProfileViewSet(viewsets.ModelViewSet):
-        """Handle creating and updating profiles"""
-        serializer_class = serializers.UserProfileSerializer
-        queryset = models.UserProfile.objects.all()
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
     
