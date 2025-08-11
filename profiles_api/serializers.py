3ad90811cf0781525ce191bcbbd876c40f5e8604
Klecianny Melo <klecianny.melo@quorum.us>
2025-08-11 20:24:03 -0300

    :sparkles: userSerializer create

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -17,3 +17,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
                 'style': {'input_type': 'password'}
             }
         }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
